6d516947c00b2857a7e0878b2ee35a554bdacf9d
Pieruigi <pierluigi.zomparelli@gmail.com>
2025-07-10 18:02:58 +0200

    wip on maze

--- Assets/Scripts/MonsterSpawner.cs
+++ Assets/Scripts/MonsterSpawner.cs
@@ -83,9 +83,8 @@ namespace TMOT
 
         public void SpawnRandomMonsters(int count)
         {
-            return;
             Debug.Log("TEST - spawn new monsters");
-
+            return;
             List<Transform> candidates = LevelController.Instance.Waypoints.ToList().FindAll(s => Vector3.Distance(PlayerController.Instance.transform.position, s.position) > spawnDistance);
             for (int i = 0; i < count; i++)
             {
