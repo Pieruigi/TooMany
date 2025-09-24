7ff79aa6147e64d6379f7ca3e2724a322f68d527
Pieruigi <pierluigi.zomparelli@gmail.com>
2025-09-24 19:00:51 +0200

    Update GameMode7.cs

--- Assets/Scripts/GameModes/GameMode7.cs
+++ Assets/Scripts/GameModes/GameMode7.cs
@@ -41,6 +41,20 @@ namespace TMOT
 
         int diamondCount = 4;
 
+        float hunterTime = 15;
+
+        float hunterExtraTime = 0;
+
+        float switchCooldown = 1;
+
+        float switchBackCooldown = 1;
+
+        float switchCooldownTimer = 0;
+
+        float elapsed = 0;
+
+
+
         protected override void Awake()
         {
             base.Awake();
@@ -52,6 +66,34 @@ namespace TMOT
             MonsterSpawner.Instance.SpawnTime = monsterRegularSpawnTime;
             Instantiate(timeUpSpawnerPrefab);
 
+        }
+
+        void Update()
+        {
+            if (GameManager.Instance.GameState != GameState.Playing) return;
+
+            elapsed += Time.deltaTime;
+
+
+            if (switchCooldownTimer > 0)
+            {
+                switchCooldownTimer -= Time.deltaTime;
+            }
+
+            if (goalProgress >= goalCount)
+            {
+                if (PlayerController.Instance.State != PlayerState.Dead)
+                {
+                    GameManager.Instance.ReportPlayerIsWinner();
+                    return;
+                }
+
+            }
+
+            CheckHunterTime();
+
+            if (Input.GetKeyDown(KeyCode.E))
+                Switch();
         }
 
         protected override void OnEnable()
@@ -147,6 +189,48 @@ namespace TMOT
                 DiamondSpawner.Instance.SpawnDiamond();
                 await UniTask.Delay(TimeSpan.FromSeconds(.25f));
             }
+        }
+
+        void CheckHunterTime()
+        {
+            if (PlayerController.Instance.State != PlayerState.Hunter) return;
+
+            hunterTime -= Time.deltaTime;
+
+            if (hunterTime < 0)
+            {
+                hunterTime = 0;
+                switchCooldownTimer = switchCooldown;
+                PlayerController.Instance.SetState(PlayerState.Prey);
+
+            }
+
+
+        }
+        
+        void Switch()
+        {
+            if (PlayerController.Instance.State == PlayerState.Prey)
+            {
+                // Still in cooldown or no chase time left
+                if (switchCooldownTimer > 0 || hunterTime == 0) return;
+
+                // Switch
+                PlayerController.Instance.SetState(PlayerState.Hunter);
+
+                switchCooldownTimer = switchBackCooldown;
+            }
+            else if (PlayerController.Instance.State == PlayerState.Hunter)
+            {
+                if (switchCooldownTimer > 0) return;
+
+                // You can always switch from hunter to prey
+                PlayerController.Instance.SetState(PlayerState.Prey);
+
+                // Set the cooldown
+                switchCooldownTimer = switchCooldown;
+            }
+
         }
          
 
