2b1892b0b514de196bd790e2db997a7b9648edf0
Pieruigi <pierluigi.zomparelli@gmail.com>
2025-10-09 14:19:37 +0200

    testing

--- Assets/Scripts/MonsterSpawner.cs
+++ Assets/Scripts/MonsterSpawner.cs
@@ -115,7 +115,7 @@ namespace TMOT
         public void SpawnRandomMonsters(int count, bool isBlue = false)
         {
 #if UNITY_EDITOR
-            //return;
+            return;
 #endif
 
             List<Transform> candidates = LevelController.Instance.Waypoints.ToList().FindAll(s => Vector3.Distance(PlayerController.Instance.transform.position, s.position) > spawnDistance);
